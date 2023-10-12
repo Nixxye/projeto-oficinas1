fe8fcdace34cc82654cb81f01b48d483bdce7016
Jean Cunha <128294977+Nixxye@users.noreply.github.com>
2023-10-12 19:44:32 -0300

    Update motor.py

--- Python/motor.py
+++ Python/motor.py
@@ -8,26 +8,24 @@ CW = 1
 CCW = 0
 SPR = 160
 
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(DIR, GPIO.OUT)
-GPIO.setup(STEP, GPIO.OUT)
-GPIO.output(DIR, CW)
-
-step_count = SPR
-delay = 0.01 
-
-#while True:
-    #GPIO.output(STEP, GPIO.HIGH)
-    #sleep(delay)
-    #GPIO.output(STEP, GPIO.LOW)
-    #sleep(delay)
-while True:
-    for i in range(100):
-        GPIO.output(STEP, GPIO.HIGH)
-        sleep(delay)
-        GPIO.output(STEP, GPIO.LOW)
-        sleep(delay)
-
-
-GPIO.cleanup()
+class Motor:
+    step_count = SPR
+    delay = 0.01
+    
+    def __init __ (self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(DIR, GPIO.OUT)
+        GPIO.setup(STEP, GPIO.OUT)
+        GPIO.output(DIR, CW)
+        
+    def exec (self):
+        while (1):
+            for i in range(100):
+                GPIO.output(STEP, GPIO.HIGH)
+                sleep(delay)
+                GPIO.output(STEP, GPIO.LOW)
+                sleep(delay)
+                
+if __name__ == '__main__':
+    motor = Motor()
+    motor.run()
