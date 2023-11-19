a7d63545b1427dfb3c07b0b2581855529b7eb525
Jean Cunha <128294977+Nixxye@users.noreply.github.com>
2023-11-19 20:19:13 -0300

    Update GuitarHealer.py

--- Python/GuitarHealer.py
+++ Python/GuitarHealer.py
@@ -1,27 +1,30 @@
 import Motor
 import T_class
 import asyncio
-import Lcd
+#import Lcd
+
 class GuitarHealer:
     def __init__(self, loop):
         self.motor = Motor.Motor()
-        self.lcd = Lcd.Lcd()
+        #self.lcd = Lcd.Lcd()
         
         self.loop = loop
         self.threads = []
         self.threads.append(loop.create_task(self.motor.run()))
-        self.threads.append(loop.create_task(self.lcd.run()))
+        #self.threads.append(loop.create_task(self.lcd.run()))
 
     def close(self):
-        T_class.close()
+        T_class.T_class.close()
         for thread in self.threads:
             thread.cancel()
         self.loop.stop()
         self.loop.close()
+        del self.motor
+
+    def __del__(self):
+        self.close()
+                
 
 loop = asyncio.get_event_loop()
 game = GuitarHealer(loop)
 loop.run_forever()
-T_class.close()
-loop.close()
-
