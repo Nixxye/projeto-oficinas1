9380b43c1bb7d9d48c072180dcade5a9363a451f
Nixxye <majornix051@gmail.com>
2023-11-21 09:49:25 -0300

    Leds em threads

--- Python/GuitarHealer.py
+++ Python/GuitarHealer.py
@@ -1,25 +1,27 @@
-import Motor
+#import Motor
 import T_class
 import asyncio
 #import Lcd
+import Led
 
 class GuitarHealer:
     def __init__(self, loop):
-        self.motor = Motor.Motor()
+        #self.motor = Motor.Motor()
         #self.lcd = Lcd.Lcd()
-        
+        self.led = Led.Led()
         self.loop = loop
         self.threads = []
-        self.threads.append(loop.create_task(self.motor.run()))
+        #self.threads.append(loop.create_task(self.motor.run()))
         #self.threads.append(loop.create_task(self.lcd.run()))
-
+        self.threads.append(loop.create_task(self.led.run()))
+        
     def close(self):
         T_class.T_class.close()
         for thread in self.threads:
             thread.cancel()
         self.loop.stop()
         self.loop.close()
-        del self.motor
+        #del self.motor
 
     def __del__(self):
         self.close()
---
+++ Python/Led.py
@@ -0,0 +1,51 @@
+import time
+import board
+import neopixel
+import asyncio
+
+import T_class
+
+class Led(T_class.T_class):
+    def __init__(self):
+        # NeoPixels must be connected to D10, D12, D18 or D21 to work.
+        self.pixel_pin = board.D21 
+        self.num_pixels = 19
+        # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+        # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+        self.ORDER = neopixel.GRB
+        self.pixels = neopixel.NeoPixel(
+            self.pixel_pin, self.num_pixels, brightness=0.2, auto_write=False, pixel_order=self.ORDER
+        )
+    
+    async def run(self):
+        while not T_class.T_class.end:
+            await self.rainbow_cycle(0.001)
+            await asyncio.sleep(0.01)
+    def wheel(self, pos):
+        # Input a value 0 to 255 to get a color value.
+        # The colours are a transition r - g - b - back to r.
+        if pos < 0 or pos > 255:
+            r = g = b = 0
+        elif pos < 85:
+            r = int(pos * 3)
+            g = int(255 - pos * 3)
+            b = 0
+        elif pos < 170:
+            pos -= 85
+            r = int(255 - pos * 3)
+            g = 0
+            b = int(pos * 3)
+        else:
+            pos -= 170
+            r = 0
+            g = int(pos * 3)
+            b = int(255 - pos * 3)
+        return (r, g, b) if self.ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+
+    async def rainbow_cycle(self, wait):
+        for j in range(255):
+            for i in range(self.num_pixels):
+                pixel_index = (i * 256 // self.num_pixels) + j
+                self.pixels[i] = self.wheel(pixel_index & 255)
+            self.pixels.show()
+            await asyncio.sleep(wait)
